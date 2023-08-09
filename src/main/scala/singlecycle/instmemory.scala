0b6090b94ea596edae00893605182720d0e5e0da
Myrausman <maira.usman5703@gmail.com>
2023-08-09 12:50:10 +0500

    vsetvli path

---
+++ src/main/scala/singlecycle/instmemory.scala
@@ -0,0 +1,17 @@
+package singlecycle
+import chisel3 . _
+import chisel3 . util . _
+import chisel3.util.experimental.loadMemoryFromFile
+import scala.io.Source
+class InstMem extends Module {
+  val io = IO (new Bundle {
+    val addr = Input ( UInt ( 10 . W ) )
+    val inst = Output ( UInt ( 32 . W ) )
+    }
+  )
+
+val imemm = Mem ( 1024 , UInt ( 32 . W ) )
+
+loadMemoryFromFile (imemm,"/home/maira/Arsh-SingleCycleCore/src/main/scala/singlecycle/abc.txt")
+io.inst := imemm.read(io.addr)
+}
