0c14b5719b368cb347893c7590a18f4305faae5f
latifbhatti <latifbhatti012@gmail.com>
2024-09-06 12:13:06 +0500

    error solved for vector to scalar(sew = 8)

--- src/main/scala/vaquita/excute_stage.scala
+++ src/main/scala/vaquita/excute_stage.scala
@@ -58,11 +58,37 @@ class excute_stage(implicit val config: Vaquita_Config) extends Module {
     io.ex_instr_out := RegNext(io.ex_instr_in)
     vec_alu_module.io.vl := RegNext(io.vl_in)
     vec_alu_module.io.vl_in   := vsetvli_module.io.vl
+
+    def sew_selector_with_element(sew:UInt,rs1:SInt):SInt={
+
+        val element_return = WireInit(0.S(config.XLEN.W))
+        val sew8_element  =   WireInit(0.S(8.W))
+        val sew16_element =  WireInit(0.S(16.W))
+        val sew32_element =  WireInit(0.S(32.W))
+        // val sew64_element =  WireInit(0.S(32.W))
+        sew8_element  := rs1(7,0).asSInt
+        sew16_element := rs1(15,0).asSInt
+        sew32_element := rs1
+        // sew64_element := rs1
+
+        when(sew==="b000".U){
+           element_return:= Cat(sew8_element,sew8_element,sew8_element,sew8_element).asSInt
+        }.elsewhen(sew==="b001".U){
+           element_return:= Cat(sew16_element,sew16_element).asSInt
+        }.elsewhen(sew==="b010".U){
+           element_return:= sew32_element
+        }
+        .otherwise{
+            element_return:= 0.S
+        }
+        element_return
+    }
+
     for (i <- 0 to 7) { // for grouping = 8
   for (j <- 0 until (config.count_lanes)) {
     // vs1_reg_data(i)(j) := io.ex_vs1_data_in(i)(j)
     // vs2_reg_data(i)(j) := io.ex_vs2_data_in(i)(j)
-    vec_alu_module.io.vs1_in(i)(j) := Mux(io.ex_instr_out(6,0)==="b1010111".U && io.ex_instr_out(14,12)==="b100".U,io.hazard_rs1.asSInt,io.ex_vs1_data_in(i)(j))
+    vec_alu_module.io.vs1_in(i)(j) := Mux(io.ex_instr_out(6,0)==="b1010111".U && io.ex_instr_out(14,12)==="b100".U,sew_selector_with_element(next_sew,io.hazard_rs1.asSInt),io.ex_vs1_data_in(i)(j))
     vec_alu_module.io.vs2_in(i)(j) := io.ex_vs2_data_in(i)(j)
     vec_alu_module.io.vs3_in(i)(j) := io.ex_vs3_data_in(i)(j)
     vec_alu_module.io.vs0_in(i)(j) := io.ex_vs0_data_in(i)(j)
