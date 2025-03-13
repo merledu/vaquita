0fe31953eb4e16a223757c6f380400eadc6fa677
latifbhatti <latifbhatti012@gmail.com>
2025-03-13 16:15:55 +0500

    removed uncommented code

--- src/main/scala/vaquita/VecMemFetch.scala
+++ src/main/scala/vaquita/VecMemFetch.scala
@@ -14,12 +14,9 @@ class VecMemFetch(implicit val config: VaquitaConfig) extends Module {
     val dccmReq = Decoupled(new MemRequestIO)
     val dccmRsp = Flipped(Decoupled(new MemResponseIO))
     val mem_lmul_in = Input(UInt(32.W))
-    // val vec_comparison_bit = Input(Bool())
   })
   io.dccmRsp.ready := true.B
   io.dccmReq.bits.activeByteLane := "b1111".U
-  // io.dccmReq.bits.dataRequest := 0.U
-  // io.dccmReq.bits.addrRequest := io.addr+offset
   io.dccmReq.bits.isWrite := io.write_en
   io.dccmReq.valid := true.B
 val lmul = WireInit(1.U(32.W))
@@ -30,22 +27,11 @@ val lmul = WireInit(1.U(32.W))
     is(3.U) { lmul := 8.U }
   }
 
-//   // when(io.read_en===1.B){
-//     for (i <- 0 to 7) { // for grouping = 8
-//       for (j <- 0 until (config.vlen >> 6)) {
-//         io.vec_read_data_load(i)(j) := WireInit(0.S(32.W))// io.dccmRsp.bits.dataResponse.asSInt
-//       }
-//     }
-// io.vec_read_data_load(0)(0) := io.dccmRsp.bits.dataResponse.asSInt
-
 //-----------vector address ---------------------------start
   val vec_load_store_bit = io.read_en || io.write_en
   dontTouch(vec_load_store_bit)
-
   val vec_load_store_counter = RegInit(((config.vlen.U*2.U)/32.U)-1.U(32.W))
   val offset = RegInit(0.U(32.W))
-  // val lmul =io.mem_lmul_in
-
   dontTouch(vec_load_store_counter)
   val vec_stall = WireInit(false.B)
   when(vec_load_store_bit===1.B && vec_load_store_counter=/=0.U){
@@ -53,7 +39,6 @@ val lmul = WireInit(1.U(32.W))
     vec_stall := true.B
     offset := offset + 4.U
   }.otherwise{
-    // vec_load_store_counter := RegInit(((config.vlen.U*2.U)/32.U)-1.U(32.W))
     when (lmul==="b000".U){
       vec_load_store_counter := ((config.vlen.U*1.U)/32.U)-1.U
     }
@@ -78,62 +63,17 @@ val lmul = WireInit(1.U(32.W))
 //-----------vector address ---------------------------end
 
 
-//------------data send to memory-----------------
-
-
-  // when(io.write_en){
-  //     io.dccmReq.bits.dataRequest := io.mem_vs3_data(0)(0).asUInt
-  // }.otherwise{
-  //   io.dccmReq.bits.dataRequest := 0.U
-  // }
-
-
-      // }
-    // }.otherwise{
-    //     for (i <- 0 to 7) { // for grouping = 8
-    //   for (j <- 0 until (config.vlen >> 6)) {
-    //     io.vec_read_data_load(i)(j) := 0.S
-    //   }
-    //   }
-
-      // }
-
-      
-      
-  //   val write_data = io.mem_vs3_data(0)(0).asUInt
-
-  // when(io.write_en){
-  //   printf("%x\n", write_data(31,0))
-  // }
-
 
 
   //---------------data read from memory-------------------
 
-    // val initValue    = VecInit(Seq.fill(8)(VecInit(Seq.fill(config.count_lanes)(0.S(config.XLEN.W)))))
     val vsd_data_reg = RegInit(VecInit(Seq.fill(8){VecInit(Seq.fill(config.count_lanes) {0.S(config.XLEN.W)})}))
-
-    // val vsd_data_reg = RegNext(initValue)
-
-    // for (i <- 0 to 7) { // for grouping = 8
-    //     for (j <- 0 until (config.count_lanes)) {
-    //         vsd_data(i)(j) := io.mem_vsd_data_in(i)(j)
-    //         io.mem_vsd_data_out(i)(j) := vsd_data(i)(j)
-    //         // vs3_data(i)(j) := io.mem_vs1_data_vs3_in(i)(j)
-    //         io.vs3_data_out(i)(j) := io.mem_vs1_data_vs3_in(i)(j)// vs3_data(i)(j)
-    //         // io.vec_read_data_load(i)(j) := 0.S
-    //       }
-    // }
-
-    // ---------------- reg store
-
 
     val load_reg_i          = RegInit(0.U(32.W))
     val load_reg_j          = RegInit(0.U(32.W))
     val load_reg_end        = RegInit(((config.vlen.U)/32.U)-1.U(32.W))
     val load_lmul           = lmul//io.mem_lmul_in
     val read_data_last_reg = WireInit(0.S(32.W))
-    // val check_i             = WireInit(0.U(32.))
     dontTouch(load_reg_i)
     dontTouch(load_reg_j)
     dontTouch(load_reg_end)
@@ -161,10 +101,10 @@ val lmul = WireInit(1.U(32.W))
     load_reg_i := 0.U
   }
 
-  when(load_reg_i+1.U===lmul && load_reg_j===(config.vlen.U/32.U-1.U)){//vec changes
+  when(load_reg_i+1.U===lmul && load_reg_j===(config.vlen.U/32.U-1.U)){
   for (i <- 0 to 7) { // for grouping = 8
       for (j <- 0 until (config.count_lanes)) {
-        io.vec_read_data_load(i)(j) := vsd_data_reg(i)(j)// io.dccmRsp.bits.dataResponse.asSInt
+        io.vec_read_data_load(i)(j) := vsd_data_reg(i)(j)
       }
     }
     //vec changes--------------------------
@@ -172,7 +112,7 @@ val lmul = WireInit(1.U(32.W))
   }.otherwise{
     for (i <- 0 to 7) { // for grouping = 8
       for (j <- 0 until (config.count_lanes)) {
-        io.vec_read_data_load(i)(j) := 0.S// io.dccmRsp.bits.dataResponse.asSInt
+        io.vec_read_data_load(i)(j) := 0.S
       }
     }
   }
@@ -195,9 +135,6 @@ val store_reg_i          = RegInit(0.U(32.W))
         }.otherwise{
           io.dccmReq.bits.dataRequest  := io.mem_vs3_data(store_reg_i)((config.vlen.U/32.U-1.U)).asUInt
           printf("%x\n", io.mem_vs3_data(store_reg_i)((config.vlen.U/32.U-1.U)).asUInt)
-          // for (i <- 1 to 56){
-            // printf("%x\n","hdeadbeef".U)
-          // }
           store_reg_j := 0.U
           store_reg_i := store_reg_i +1.U
         }
@@ -213,27 +150,4 @@ val store_reg_i          = RegInit(0.U(32.W))
     store_reg_i := 0.U
     io.dccmReq.bits.dataRequest  := 0.U
   }
-
-// //---------check code-----------------start
-//   when(io.write_en){
-//       when(reg_i=/=lmul){
-//         when(reg_j=/=reg_end){
-//           when(reg_k===0.B){
-//             io.dccmReq.bits.dataRequest  := io.mem_vs3_data(reg_i)(reg_j)(31,0).asUInt
-//             printf("%x\n", io.mem_vs3_data(reg_i)(reg_j).asUInt)
-//             reg_k := 1.B
-//           }.otherwise{
-//             io.dccmReq.bits.dataRequest  := io.mem_vs3_data(reg_i)(reg_j).asUInt
-//             printf("%x\n", io.mem_vs3_data(reg_i)(reg_j).asUInt)
-//             reg_k := 0.B
-//           }
-//         }.otherwise{
-//           io.dccmReq.bits.dataRequest  := 0.U
-//         }
-//       }.otherwise{
-//         io.dccmReq.bits.dataRequest  := 0.U
-//       }
-//   }.otherwise{
-//     io.dccmReq.bits.dataRequest := 0.U
-//   }
 }
