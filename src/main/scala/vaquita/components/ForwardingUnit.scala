ea24c1f9963e3321466c1662c20eea2c5cc2f153
latifbhatti <latifbhatti012@gmail.com>
2025-03-23 01:30:13 +0500

    optimized code of forwarding unit

--- src/main/scala/vaquita/components/ForwardingUnit.scala
+++ src/main/scala/vaquita/components/ForwardingUnit.scala
@@ -2,7 +2,6 @@
 package vaquita.components
 import chisel3._
 
-
 class ForwardingUnit extends Module {
   val io = IO(new Bundle {
     val mem_vd       = Input(UInt(5.W))
@@ -21,40 +20,21 @@ class ForwardingUnit extends Module {
   val vs3_addr     = io.instr_in(11,7)
   val vs0_addr_bit = vs1_addr | vs2_addr
   val func3        = io.instr_in(14,12)
-  dontTouch(func3)
+
+  def forwardLogic(src: UInt, mem_vd: UInt, wb_vd: UInt, mem_regWrite: Bool, wb_regWrite: Bool, func3: UInt = 0.U, checkFunc3: Bool = false.B): UInt = {
+    Mux((src === mem_vd && (!checkFunc3 || func3 === 0.U)) && mem_vd =/= 0.U && mem_regWrite, 1.U,
+      Mux((src === wb_vd && (!checkFunc3 || func3 === 0.U)) && wb_vd =/= 0.U && wb_regWrite, 2.U, 0.U)
+    )
+  }
+
+  io.forward_a := forwardLogic(vs1_addr, io.mem_vd, io.wb_vd, io.mem_regWrite, io.wb_regWrite, func3, checkFunc3 = true.B)
+  io.forward_b := forwardLogic(vs2_addr, io.mem_vd, io.wb_vd, io.mem_regWrite, io.wb_regWrite)
+  io.forward_c := forwardLogic(vs3_addr, io.mem_vd, io.wb_vd, io.mem_regWrite, io.wb_regWrite)
+  io.forward_d := forwardLogic(0.U, io.mem_vd, io.wb_vd, io.mem_regWrite, io.wb_regWrite)
 
   io.forward_a := DontCare
   io.forward_b := DontCare
-  // forword A => vs1 data Hazard
-  when((vs1_addr === io.mem_vd && 0.U===func3) && io.mem_vd =/= 0.U && io.mem_regWrite) { // fun3 for vector to vector in vs1(solve immediate number and scalar number)
-    io.forward_a := 1.U
-  }.elsewhen( vs1_addr === io.wb_vd && 0.U===func3 && io.wb_vd =/= 0.U && io.wb_regWrite) {
-    io.forward_a := 2.U
-  }.otherwise {
-    io.forward_a := 0.U
-  }
-  // forword B => vs2 data Hazard
-  when((vs2_addr === io.mem_vd) && io.mem_vd =/= 0.U && io.mem_regWrite) {
-    io.forward_b := 1.U
-  }.elsewhen( vs2_addr === io.wb_vd && io.wb_vd =/= 0.U && io.wb_regWrite) {
-    io.forward_b := 2.U
-  }.otherwise {
-    io.forward_b := 0.U
-  }
-  // forword c => vs3 data Hazard
-  when((vs3_addr === io.mem_vd) && io.mem_vd =/= 0.U && io.mem_regWrite) {
-    io.forward_c := 1.U
-  }.elsewhen( vs3_addr === io.wb_vd && io.wb_vd =/= 0.U && io.wb_regWrite) {
-    io.forward_c := 2.U
-  }.otherwise {
-    io.forward_c := 0.U
-  }
-  // forword D => vs0 data Hazard
-  when((0.U === io.mem_vd && vs0_addr_bit===1.B) && io.mem_vd =/= 0.U && io.mem_regWrite) {
-    io.forward_d := 1.U
-  }.elsewhen( (0.U === io.wb_vd && vs0_addr_bit===1.B) && io.wb_vd =/= 0.U && io.wb_regWrite) {
-    io.forward_d := 2.U
-  }.otherwise {
-    io.forward_d := 0.U
-  }
+  io.forward_c := DontCare
+  io.forward_d := DontCare
+
 }
